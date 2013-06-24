bff770bac347648a31c2e22217b5f7f5f87c7f75
woohaha <realwoohaha@gmail.com>
2013-06-24 21:06:37 +0800

    更新了__get_xml()方法的xml地址，屏蔽了下載功能，衹顯示歌曲地址

--- xiamidownloader.py
+++ xiamidownloader.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-                             
+
 import re, urllib, urllib2
-                             
+
 class XiamiDownload(object):
-                             
+
     """虾米音乐下载"""
-                             
+
     def __init__(self, url_song):
-                             
+
         """ 初始化，得到请求xml和加密的下载地址 """
-                             
-        self.url_song = url_song      
+
+        self.url_song = url_song
         self.url_xml = self.__get_xml()
         self.info = self. __get_info()
         self.url_location = self.info[0]
         self.lyc = self.info[1]
         self.pic = self.info[2]
-                                 
+
     def __get_xml(self):
-                             
+
         """ 得到请求的 xml 地址 """
-                             
-        return 'http://www.xiami.com/song/playlist/id/%s/object_name/default/object_id/0' % re.search('\d+', self.url_song).group()
-                             
+
+        print u'xml地址是: http://www.xiami.com/song/playlist/id/%s/type/1' % re.search('\d+', self.url_song).group()
+        return 'http://www.xiami.com/song/playlist/id/%s/type/1' % re.search('\d+', self.url_song).group()
+
     def __get_info(self):
-                             
+
         """ 伪装浏览器请求，处理xml，得到 加密的 location """
-                             
+
         headers = {
             'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
         }
@@ -43,18 +44,18 @@ class XiamiDownload(object):
         lyc = re.search(r'(<lyric>)(.+)(<\/lyric>)', xml).group(2)
         pic = re.search(r'(<pic>)(.+)(<\/pic>)', xml).group(2)
         return (location, lyc, pic)
-                             
+
     def get_url(self):
-                             
+
         """ 解密 location 获得真正的下载地址 """
-                             
+
         strlen = len(self.url_location[1:])
         rows = int(self.url_location[0])
         cols = strlen / rows
         right_rows = strlen % rows
         new_str = self.url_location[1:]
         url_true = ''
-                             
+
         for i in xrange(strlen):
             x = i % rows
             y = i / rows
@@ -65,24 +66,24 @@ class XiamiDownload(object):
                 p = right_rows * (cols + 1) + (x - right_rows) * cols + y
             url_true += new_str[p]
         return urllib2.unquote(url_true).replace('^', '0')
-                             
+
 if __name__ == '__main__':
-                             
+
     url = raw_input('please enter the url of the song: ')
     xi = XiamiDownload(url)
     url_download = xi.get_url()
     url_pic = xi.pic
     url_lyc = xi.lyc
-                             
+
     print u'下载地址是: %s' % url_download
-                             
-    print u"下载开始..."
-                             
-    try:
-        urllib.urlretrieve(url_download, r'new.mp3')
-        urllib.urlretrieve(url_pic, r'new.jpg')
-        urllib.urlretrieve(url_lyc, r'new.lyc')
-    except:
-        raise(u'请求错误，请重试')
-                                 
-    print u"完成下载..."
+
+#    print u"下载开始..."
+#
+#    try:
+#        urllib.urlretrieve(url_download, r'new.mp3')
+#        urllib.urlretrieve(url_pic, r'new.jpg')
+#        urllib.urlretrieve(url_lyc, r'new.lyc')
+#    except:
+#        raise(u'请求错误，请重试')
+#
+#    print u"完成下载..."
